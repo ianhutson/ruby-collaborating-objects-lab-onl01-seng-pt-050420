939a5b096dd3700a458949e2db518bd4cf29f96b
Ian Hutson <ih00209@georgiasouthern.edu>
2020-05-29 22:27:26 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,21 @@
+def Artist
+attr_accessor :name, :songs
+@@all = []
+
+def initialize(name)
+  @name = name
+  save
+end
+
+def name=(name)
+end
+
+def self.all
+  @@all
+end
+
+def save
+  @@all < self
+end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,3 @@
+def Song
+attr_accessor :artist,  
+end
